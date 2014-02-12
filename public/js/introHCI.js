2b50427140e6252a0c1980c56b0181055f4f3381
Alejandro Rodriguez <arodrig1@stanford.edu>
2014-02-12 23:47:50 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,23 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+	$('#feelingBtn').click(randomFeeling);
+}
+
+function randomFeeling(e) {
+	e.preventDefault();
+	var data = {
+		returnfields : 'sentence',
+		limit : 1,
+		display : 'text',
+		jsonp : insertFeelingCallback
+	};
+	$.getJSON("http://api.wefeelfine.org:8080/ShowFeelings", data);
+}
+
+function insertFeelingCallback(result) {
+	console.log(result);
 }
 
 /*
@@ -27,6 +44,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, insertDetailsCallback);
+}
+
+function insertDetailsCallback(result) {
+	//console.log(result);
+	var projectID = result['id'];
+	var projectDiv = $('#project' + projectID).find('.details');
+	var title = "<h3>" + result['title'] + "</h3>";
+	var date = "<h4>" + result['date'] + "</h4>";
+	var image = "<img class='detailsImage' src='" + result['image'] + "'>";
+	projectDiv.html(title + date + image + result['summary']);
 }
 
 /*
@@ -34,5 +63,18 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	//console.log("User clicked on color button");
+	e.preventDefault();
+
+	$.get("/palette", changeColorsCallback);
+
+}
+
+function changeColorsCallback(result) {
+	var colors = result['colors']['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
